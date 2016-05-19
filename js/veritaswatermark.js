b80ab3040cf160a15f204b8e8dbcbc72b8125eba
Luca Weiss <WEI16416@spengergasse.at>
2016-05-19 15:02:16 +0200

    Initial commit

---
+++ js/veritaswatermark.js
@@ -0,0 +1,30 @@
+var manifest = chrome.runtime.getManifest();
+var args = ["%c Veritas Watermark Remover%c v" + manifest.version + "%c by%c " + manifest.author + " %c " + manifest.homepage_url, "background: #222;color: #bada55", "background: #222;color: #c0ffee", "background: #222;color: #bada55", "background: #222;color: #c0ffee", ""];
+console.log.apply(console, args);
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+	remove_watermark();
+});
+
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
+
+if(window.location.href.match(/.*veritas.at\/sbo\/ebook\/px\/[0-9]*\/files\/assets\/basic-html/)) {
+	setTimeout(remove_watermark, 100);
+}
+
+function remove_watermark() {
+	var maybe_watermark = $("#Page > div:last").html();
+	if(maybe_watermark == null) {
+		return;
+	}
+	if(maybe_watermark.indexOf("Eigentum des VERITAS Verlags") >= 0) {
+		$("#Page > div:last").remove();
+		$("#Page > div:last").remove();
+	}
+
+}
