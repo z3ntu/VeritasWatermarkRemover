7a177bcab7035f4693e37ea77c3b2f05a2f031e1
Luca Weiss <luca@z3ntu.xyz>
2017-03-24 08:05:22 +0100

    Update to 1.1: also remove watermarks with typos

--- js/veritaswatermark.js
+++ js/veritaswatermark.js
@@ -5,26 +5,28 @@ console.log.apply(console, args);
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
-	remove_watermark();
+    remove_watermark();
 });
 
 observer.observe(document, {
-  subtree: true,
-  attributes: true
+    subtree: true,
+    attributes: true
 });
 
 if(window.location.href.match(/.*veritas.at\/sbo\/ebook\/px\/[0-9]*\/files\/assets\/basic-html/)) {
-	setTimeout(remove_watermark, 100);
+    setTimeout(remove_watermark, 100);
 }
 
 function remove_watermark() {
-	var maybe_watermark = $("#Page > div:last").html();
-	if(maybe_watermark == null) {
-		return;
-	}
-	if(maybe_watermark.indexOf("Eigentum des VERITAS Verlags") >= 0) {
-		$("#Page > div:last").remove();
-		$("#Page > div:last").remove();
-	}
-
+    var maybe_watermark = $("#Page > div:last").html();
+    if(maybe_watermark == null) {
+        return;
+    }
+    // Yes, their sites have typos in the watermark... Like "Eigent m des VERIT S Verlags".
+    if(maybe_watermark.indexOf("Eigentum") >= 0 ||
+       maybe_watermark.indexOf("VERITAS") >= 0 ||
+       maybe_watermark.indexOf("Verlags") >= 0) {
+        $("#Page > div:last").remove();
+        $("#Page > div:last").remove();
+    }
 }
